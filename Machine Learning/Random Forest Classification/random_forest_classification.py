b949734e1a390983a6c75b10f8bdae7a68ce32be
dagaca <dagacaa@gmail.com>
2022-10-07 02:30:42 +0300

    random forest classification eklendi

---
+++ Machine Learning/Random Forest Classification/random_forest_classification.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  7 02:25:17 2022
+
+@author: Damla
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# import data
+data = pd.read_csv("data.csv")
+
+data.drop(["id", "Unnamed: 32"], axis = 1, inplace = True)
+
+M = data[data.diagnosis == "M"]
+B = data[data.diagnosis == "B"]
+
+data.diagnosis = [1 if each == "M" else 0 for each in data.diagnosis]
+y = data.diagnosis.values
+x_data = data.drop(["diagnosis"], axis=1)
+
+# normalization
+x = (x_data - np.min(x_data)) / (np.max(x_data) - np.min(x_data))
+
+# train test split
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.15, random_state = 42)
+
+#%% Decision tree 
+from sklearn.ensemble import RandomForestClassifier
+
+rf = RandomForestClassifier(n_estimators = 100, random_state = 1)
+rf.fit(x_train,y_train)
+
+#%% test
+print("Accuracy of Random Forest Algorithm: ", rf.score(x_test,y_test))
