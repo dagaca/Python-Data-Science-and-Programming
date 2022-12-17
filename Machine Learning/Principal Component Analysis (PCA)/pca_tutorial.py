e384f68fb3b6e838a340e695805f6dea7312a610
dagaca <dagacaa@gmail.com>
2022-12-18 01:01:39 +0300

    pca güncellendi

--- Machine Learning/Principal Component Analysis (PCA)/pca_tutorial.py
+++ Machine Learning/Principal Component Analysis (PCA)/pca_tutorial.py
@@ -30,3 +30,19 @@ x_pca = pca.transform(x)
 print("variance ratio: ", pca.explained_variance_ratio_)
 
 print("sum: ", sum(pca.explained_variance_ratio_))
+
+# %% 2D Visualization
+
+df["p1"] = x_pca[:,0]
+df["p2"] = x_pca[:,1]
+
+color = ["red","green","blue"]
+
+import matplotlib.pyplot as plt
+for each in range(3):
+    plt.scatter(df.p1[df.sinif == each], df.p2[df.sinif == each], color = color[each], label = iris.target_names[each])
+    
+plt.legen()
+plt.xlabel("p1")
+plt.ylabel("p2")
+plt.show()
